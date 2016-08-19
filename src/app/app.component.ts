f1ca34e9ae91cfb479afc6840dd161902e254cd1
josiah1888 <josiah@jburchard.com>
2016-08-19 13:08:43 -0500

    feat(ng2-migration): remove starter code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,36 +3,6 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Http } from '@angular/http';
 import { PropertyService } from './properties/property.service';
 
-// templateUrl example
-import { Home } from './home';
-//
-/////////////////////////
-// ** Example Directive
-// Notice we don't touch the Element directly
-
-@Directive({
-  selector: '[x-large]'
-})
-export class XLarge {
-  constructor(element: ElementRef, renderer: Renderer) {
-    // ** IMPORTANT **
-    // we must interact with the dom through -Renderer-
-    // for webworker/server to see the changes
-    renderer.setElementStyle(element.nativeElement, 'fontSize', 'x-large');
-    // ^^
-  }
-}
-
-/////////////////////////
-// ** Example Components
-@Component({
-  selector: 'about',
-  template: `
-    <div>This is the "About" page</div>
-  `
-})
-export class About { }
-
 /////////////////////////
 // ** MAIN APP COMPONENT **
 @Component({
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { RouterConfig } from '@angular/router';
-import { Home } from './home';
-import { About } from './app.component';
 import { Welcome } from './welcome/welcome.component';
 
 export const routes: RouterConfig = [
   { path: '', component: Welcome },
-  { path: 'home', component: Home },
-  { path: 'about', component: About },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: '' }
 ];
