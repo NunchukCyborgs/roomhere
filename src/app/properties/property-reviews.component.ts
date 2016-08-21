7c224ad203acfbf76d953ff31b2a0e1b1cc876b0
josiah1888 <josiah@jburchard.com>
2016-08-21 13:04:35 -0500

    fix(property-reviews): remove extra end tag

--- src/app/properties/property-reviews.component.ts
+++ src/app/properties/property-reviews.component.ts
@@ -7,38 +7,34 @@ import { PropertyService, Property } from './property.service';
   styles: [`
   `],
   template: `
-      <div>
-        <h4>Reviews</h4>
-            <div class="media-object stack-for-small">
-              <div class="media-object-section"><img class="thumbnail" src="http://placehold.it/200x200"></div>
-              <div class="media-object-section">
-                <h5>Mike Stevenson</h5>
-                <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is
-                  like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define
-                  or destroy you.</p>
-              </div>
-            </div>
-            <div class="media-object stack-for-small">
-              <div class="media-object-section"><img class="thumbnail" src="http://placehold.it/200x200"></div>
-              <div class="media-object-section">
-                <h5>Mike Stevenson</h5>
-                <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is
-                  like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define
-                  or destroy you</p>
-              </div>
-            </div>
-            <div class="media-object stack-for-small">
-              <div class="media-object-section"><img class="thumbnail" src="http://placehold.it/200x200"></div>
-              <div class="media-object-section">
-                <h5>Mike Stevenson</h5>
-                <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is
-                  like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define
-                  or destroy you</p>
-              </div>
-            </div><label>My Review
-                <textarea placeholder="None"></textarea></label> <button class="button">Submit Review</button>
+    <div>
+      <h4>Reviews</h4>
+      <div class="media-object stack-for-small">
+          <div class="media-object-section"><img class="thumbnail" src="http://placehold.it/200x200"></div>
+          <div class="media-object-section">
+              <h5>Mike Stevenson</h5>
+              <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define or destroy you.</p>
           </div>
       </div>
+      <div class="media-object stack-for-small">
+          <div class="media-object-section"><img class="thumbnail" src="http://placehold.it/200x200"></div>
+          <div class="media-object-section">
+              <h5>Mike Stevenson</h5>
+              <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define or destroy you</p>
+          </div>
+      </div>
+      <div class="media-object stack-for-small">
+          <div class="media-object-section"><img class="thumbnail" src="http://placehold.it/200x200"></div>
+          <div class="media-object-section">
+              <h5>Mike Stevenson</h5>
+              <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define or destroy you</p>
+          </div>
+      </div>
+      <label>My Review
+          <textarea placeholder="None"></textarea>
+      </label>
+      <button class="button">Submit Review</button>
+    </div>
   `
 })
 export class PropertyReviews {
