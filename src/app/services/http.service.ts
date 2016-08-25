e7549453105b68580dc2d4b5576babf7145cd6c7
josiah1888 <josiah@jburchard.com>
2016-08-24 20:40:32 -0500

    feat(http): update headers on requests

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { BASE_URL } from '../config';
+import 'rxjs';
 
 @Injectable()
 export class HttpService {
@@ -13,14 +14,25 @@ export class HttpService {
   }
 
   public get(url: string): Observable<any> {
-    return this.http.get(url, { headers: this.headers });
+    return this.http.get(url, { headers: this.headers })
+      .do(i => this.updateHeaders(i.headers));
   }
 
   public post(url: string, obj: any): Observable<any> {
-    return this.http.post(url, obj, { headers: this.headers });
+    return this.http.post(url, obj, { headers: this.headers })
+      .do(i => this.updateHeaders(i.headers));
   }
 
   public patch(url: string, obj: any): Observable<any> {
-    return this.http.patch(url, obj, { headers: this.headers });
+    return this.http.patch(url, obj, { headers: this.headers })
+      .do(i => this.updateHeaders(i.headers));
+  }
+
+  private updateHeaders(headers: Headers) {
+    headers.forEach((values: string[], name: string) => {
+      if (this.headers.keys().indexOf(name) !== -1) {
+        this.headers.set(name, values);
+      }
+    });
   }
 }
