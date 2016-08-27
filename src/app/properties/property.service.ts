bd59fc8bebba479c49763e7a7e14a2cc7ed797ba
josiah1888 <josiah@jburchard.com>
2016-08-27 13:46:12 -0500

    fix(filtered_results): update for total_results change

--- src/app/properties/property.service.ts
+++ src/app/properties/property.service.ts
@@ -4,61 +4,25 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 import { Property, PropertyFacet } from './index';
-
 import { BASE_URL } from '../config';
 
 @Injectable()
 export class PropertyService {
-  public collection$: BehaviorSubject<Property[]>;
-  public _collection: Property[] = [];
 
   public lastPage$: BehaviorSubject<number> = new BehaviorSubject(Number.MAX_SAFE_INTEGER)
 
-  constructor(private http: HttpService) {
-    this.collection$ = new BehaviorSubject(this._collection);
-    this.collection$.subscribe();
-
-    this.http.get(`${BASE_URL}/properties/filtered_results`)
-      .subscribe(res => {
-          this._collection = this._collection || [];
-          let json = res.json();
-
-          for (let property in json) {
-              this._collection.push(json[property]);
-              this.collection$.next(this.collection);
-          }
-      });
-  }
-    
-  get collection(): Property[] {
-    return Object.assign([], this._collection);
-  }
+  constructor(private http: HttpService) { }
 
   public getFilteredProperties$(facet: PropertyFacet, pageNumber: number = 1, perPage: number = 10): Observable<Property[]> {
-    return this.http.post(`${BASE_URL}/properties/filtered_results`, {facets: facet, page: pageNumber, per_page: perPage})
+    return this.http.post(`${BASE_URL}/properties/filtered_results`, { facets: facet, page: pageNumber, per_page: perPage })
       .map(i => i.json())
-      .do(() => this.updateLastPage(facet, pageNumber, perPage));
-  }
-
-  private updateLastPage(facet: PropertyFacet, pageNumber: number, perPage: number) {
-    // This was a best effort attempt. Really needs to just come back from the API
-    this.http.post(`${BASE_URL}/properties/filtered_results`, {facets: facet, page: pageNumber, per_page: perPage})
-      .map(i => i.json())
-      .subscribe(i => { if (i.length === 0) { this.lastPage$.next(pageNumber - 1);} });
+      .do(i => this.lastPage$.next(Math.ceil(i.total_count / perPage)))
+      .map(i => i.results);
   }
 
   public getPropertyBySlug$(slug: string): Observable<Property> {
-    let property$ = new Observable()
-    const index = this._collection.map(i => i.slug).indexOf(slug);
-    if (index !== -1) {
-      return Observable.of(this._collection[index]);
-    } else {
-      return this.http.get(`${BASE_URL}/properties/${slug}`).map(i => {
-        let property = i.json();
-        this._collection.push(property);
-        return property;
-      });
-    }
+    return this.http.get(`${BASE_URL}/properties/${slug}`)
+      .map(i => i.json());
   }
 
   public update(property: Property): Observable<Property> {
