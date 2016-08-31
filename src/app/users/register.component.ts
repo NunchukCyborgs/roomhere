3cdb3d646005437c344a95e6c5e0275ace245fad
Zach <zoo.york4130@gmail.com>
2016-08-30 19:11:59 -0500

    login_styling
    HTML and styling changes to the login and register components.  I am
    missing something small in Angular to get this to submit correctly.
    Josiah needs to go through this.

--- src/app/users/register.component.ts
+++ src/app/users/register.component.ts
@@ -12,48 +12,108 @@ declare let $: any;
   selector: 'register',
   directives: [REACTIVE_FORM_DIRECTIVES, FORM_DIRECTIVES, ControlMessages],
   styles: [`
-  
+.signup-panel {
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 15px;
+  margin-top: 30px;
+}
+.signup-panel i {
+  font-size: 30px;
+  line-height: 50px;
+  color: #999;
+}
+.signup-panel form input, .signup-panel form span {
+  height: 50px;
+}
+.signup-panel .welcome {
+  font-size: 26px;
+  text-align: center;
+  margin-left: 0;
+}
+.signup-panel p {
+  font-size: 13px;
+  font-weight: 200;
+}
+.signup-panel .button {
+  width: 50%;
+}
+.signup-panel form input, .signup-panel form span {
+    height: 50px;
+}
+span.prefix, label.prefix {
+    background: #f2f2f2;
+    border-color: #d9d9d9;
+    border-right: none;
+    color: #333;
+}
+.prefix, .postfix {
+    display: block;
+    position: relative;
+    z-index: 2;
+    text-align: center;
+    width: 100%;
+    padding-top: 0;
+    padding-bottom: 0;
+    border-style: solid;
+    border-width: 1px;
+    overflow: hidden;
+    font-size: 0.875em;
+    height: 2.3125em;
+    line-height: 2.3125em;
+}
+@media screen and (max-width: 39.9375em) {
+.signup-panel .button {
+    width: 90%;
+}
+}
   `],
   template: `
   <div>
     <button class="close-button" data-close aria-label="Close modal" type="button">
-      <span aria-hidden="true">&times;</span>
+        <span aria-hidden="true">&times;</span>
     </button>
-
-    <form [class.hide]="success" [formGroup]="registerForm" (ngSubmit)="onSubmit()">
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input formControlName="email" type="email" class="form-control" id="email">
-        <control-messages [control]="registerForm.controls.email"></control-messages>
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input formControlName="password" type="password" class="form-control" id="password">
-        <control-messages [control]="registerForm.controls.password"></control-messages>
-      </div>
-      <div class="form-group">
-        <label for="confirmPassword">Confirm Password</label>
-        <input formControlName="confirmPassword" type="password" class="form-control" id="confirmPassword">
-        <control-messages [control]="registerForm.controls.confirmPassword">{{getConfirmPasswordMatchMessage()}}</control-messages>
-      </div>
-      <button type="submit" class="btn btn-default" [attr.disabled]="!registerForm.valid || getConfirmPasswordMatchMessage() ? true : null">Create an Account</button>
-    </form>
-
-    <div [class.hide]="!success">
-      <h5>Success!</h5>
-
-      <p>
-        Alright, there's one last step to create an account. 
-        Please check your email for a magic activation link and click to confirm you are as human as we think you are.
-      </p>
+    <div class="row">
+        <div class="medium-10 medium-push-1 columns">
+            <div class="signup-panel">
+                <p class="welcome"> Welcome to Roomhere!</p>
+                <form [class.hide]="success" [formGroup]="registerForm" (ngSubmit)="onSubmit()">
+                    <div class="form-group row collapse">
+                        <div class="small-2 columns">
+                            <span class="prefix"><i class="icon-mail"></i></span>
+                        </div>
+                        <div class="small-10  columns">
+                            <input type="email" placeholder="email" formControlName="email" class="form-control" id="email">
+                        </div>
+                        <control-messages [control]="registerForm.controls.email"></control-messages>
+                    </div>
+                    <div class="form-group row collapse">
+                        <div class="small-2 columns ">
+                            <span class="prefix"><i class="icon-lock"></i></span>
+                        </div>
+                        <div class="small-10 columns ">
+                            <input placeholder="password" formControlName="confirmPassword" type="password" class="form-control" id="confirmPassword">
+                        </div>
+                        <control-messages [control]="registerForm.controls.confirmPassword">{{getConfirmPasswordMatchMessage()}}</control-messages>
+                    </div>
+                    <p class="text-center"><a type="submit" class="button large" [attr.disabled]="!registerForm.valid||getConfirmPasswordMatchMessage()?true:null">Create an Account!</a></p>
+                </form>
+                <p class="text-center">Already have an account? <a href="#">Login here</a>
+                </p>
+            </div>
+            <div [class.hide]="!success">
+                <h5>Success!</h5>
+                <p>Alright, there's one last step to create an account. Please check your email for a magic activation link and click to confirm you are as human as we think you are.
+                </p>
+            </div>
+            <div [class.hide]="!serverErrors.length || success">
+                <h6>Uh oh! We had a problem logging you in with those credentials.</h6>
+                <span *ngFor="let error of serverErrors">{{error}}. </span>
+            </div>
+        </div>
     </div>
 
-    <div [class.hide]="!serverErrors.length || success">
-      <h6>Uh oh! We had a problem logging you in with those credentials.</h6>
-
-      <span *ngFor="let error of serverErrors">{{error}}. </span>
-    </div>
-  </div>
+    
   `
 })
 export class Register {
