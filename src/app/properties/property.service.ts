01491b7356a50fb864f43815b732493b2346d54d
josiah1888 <josiah@jburchard.com>
2016-09-16 15:56:51 -0500

    chore(ng-2): rip out view cache
    It shouldn't be long until we get the real preboot system from universal. we will just wait for that

--- src/app/properties/property.service.ts
+++ src/app/properties/property.service.ts
@@ -1,7 +1,5 @@
 import { Renderer, Inject, Injectable } from "@angular/core";
 import { HttpService } from '../services/http.service';
-// import { DOCUMENT } from '@angular/platform-browser';
-// import { getDOM } from '@angular/platform-browser/src/dom/dom_adapter';
 
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
@@ -19,42 +17,17 @@ export class PropertyService {
   private viewCaches: string[] = [];
 
   public getFilteredProperties$(facet: PropertyFacet, pageNumber: number = 1, perPage: number = 10): Observable<Property[]> {
-    // const KEY: string = `properties_cache_p${pageNumber}_f${JSON.stringify(facet)}`
-    //   .replace(/[^a-z_0-9]/ig, '');
-
-    const cache = false; //this.getFromViewCache(KEY);
-    let sequence;
-    if (cache) {
-      sequence = Observable.of(cache);
-    } else {
-      sequence = this.http
-        .post(`${BASE_API_URL}/properties/filtered_results`, { facets: facet, page: pageNumber, per_page: perPage })
-        .map(i => i.json())
-    }
-
-    return sequence
+    return this.http
+      .post(`${BASE_API_URL}/properties/filtered_results`, { facets: facet, page: pageNumber, per_page: perPage })
+      .map(i => i.json())
       .do(i => this.lastPage$.next(Math.ceil(i.total_count / perPage)))
-      // .do(i => this.hydrateViewCache(i, KEY))
       .map(i => i.results);
   }
 
-  public getPropertyBySlug$(slug: string): Observable<Property> {
-    const KEY: string = `property_cache_${slug}`
-      .replace(/[^a-z_0-9]/ig, '');;
-
-    const cache = this.getFromViewCache(KEY);
-
-    let sequence;
-    if (cache) {
-      sequence = Observable.of(cache);
-    } else {
-      sequence = this.http
-        .get(`${BASE_API_URL}/properties/${slug}`)
-        .map(i => i.json())
-    }
-
-    return sequence
-      .do(i => this.hydrateViewCache(i, KEY))
+  public getPropertyBySlug$(slug: string): Observable<any> {
+    return this.http
+      .get(`${BASE_API_URL}/properties/${slug}`)
+      .map(i => i.json())
       .do(i => this._propertyBySlug = i)
       .do(() => this.propertyBySlug$ = this.propertyBySlug$ || new BehaviorSubject(this._propertyBySlug)) // do something else
       .flatMap(i => this.updateLocal(i));
@@ -62,7 +35,7 @@ export class PropertyService {
 
   public update(property: Property): Observable<any> {
     property = this.updateAmenities(property);
-    return this.http.patch(`${BASE_API_URL}/properties/${property.id}`, {property: property})
+    return this.http.patch(`${BASE_API_URL}/properties/${property.id}`, { property: property })
       .map(i => i.json())
       .flatMap(i => this.updateLocal(i));
   }
@@ -74,26 +47,8 @@ export class PropertyService {
   }
 
   private updateAmenities(property: Property): Property {
-    property.amenities_attributes = property.amenities.map(i => { return {id: i.id, _destroy: !i.active}; });
+    property.amenities_attributes = property.amenities.map(i => { return { id: i.id, _destroy: !i.active }; });
     return property;
-  }
-
-  private hydrateViewCache(obj: any, key: string) {
-    if (this.viewCaches.indexOf(key) === -1) {
-      this.viewCaches.push(key);
-      // const elem = this.renderer.createElement(this.document.body, 'meta');
-      // this.renderer.setElementClass(elem, key, true);
-      // this.renderer.setElementAttribute(elem, 'value', JSON.stringify(obj));
-    }
-  }
-
-  private getFromViewCache(key: string): any {
-    // if (isBrowser) {
-    //   const element = getDOM().query(`.${key}`);
-    //   return JSON.parse(element && element.getAttribute('value'));
-    // }
-
-    return undefined
   }
 
   public deleteImage(property: Property, imageId: number): Observable<any> {
@@ -102,14 +57,5 @@ export class PropertyService {
       .flatMap(i => this.updateLocal(i));
   }
 
-  constructor(
-    private http: HttpService,
-    // private renderer: RootRenderer,
-    // @Inject(DOCUMENT) private document: any
-  ) {
-    // Cache the base case. This request gets auto cancelled by angular
-    // during application init, but we need this to be view cached
-    // in our custom shitty view caching system
-    this.getFilteredProperties$(new PropertyFacet()).subscribe();
-  }
+  constructor(private http: HttpService) { }
 }
