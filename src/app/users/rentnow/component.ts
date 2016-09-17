4f310f0f668d341f667f91eff2ae1898965b93d6
Zach <zoo.york4130@gmail.com>
2016-09-17 16:22:31 -0500

    rentnow modal
    Angular Magic needed, but this is a starting point.

---
+++ src/app/users/rentnow/component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { User, UserService } from '../index';
+import { isBrowser } from 'angular2-universal';
+import { ValidationService } from '../../services/validation.service';
+import { ControlMessages } from '../../components/control-messages/component';
+
+@Component({
+  selector: 'login',
+  styles:[require('../modal/modal.component.scss').toString(), require('./styles.scss').toString()],
+  templateUrl: 'template.html'
+})
+export class Login {
+  public serverErrors: string[] = [];
+  public loginForm: any;
+
+  constructor(private userService: UserService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.init();
+    isBrowser && $(document).on('closed.zf.reveal', () => this.init());
+  }
+
+  private init() {
+    this.loginForm = this.formBuilder.group({
+      'email': ['', [Validators.required, ValidationService.emailValidator]],
+      'password': ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  public onSubmit() {
+    const user = new User({
+      email: this.loginForm.controls.email.value,
+      password: this.loginForm.controls.password.value,
+    });
+    
+    this.userService.login(user)
+      .do((res: Response) => this.serverErrors = ValidationService.getAuthErrors(res))
+      .subscribe((res: Response) => this.closeModal(res));
+  }
+
+  private closeModal(res?: Response) {
+    if (!res || res.ok) {
+      isBrowser && $('login .close-button').click();
+    }
+  }
+}
