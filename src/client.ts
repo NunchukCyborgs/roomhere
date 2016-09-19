eb7f3c2c325bcf8251c77ff0e2e3233cb4ee1592
josiah1888 <josiah@jburchard.com>
2016-09-19 09:55:04 -0500

    feat(preboot): work to preboot config

--- src/client.ts
+++ src/client.ts
@@ -4,10 +4,18 @@ import 'angular2-universal-polyfills/browser';
 // Angular 2
 import { enableProdMode} from '@angular/core';
 import { platformUniversalDynamic } from 'angular2-universal';
-
-const platformRef = platformUniversalDynamic();
-
 import { MainModule } from './main.browser';
 
+const platformRef = platformUniversalDynamic();
+enableProdMode();
+
+console.log('beginning...');
+
 // on document ready bootstrap Angular 2
-document.addEventListener('DOMContentLoaded', () => platformRef.bootstrapModule(MainModule));
+document.addEventListener('DOMContentLoaded', () => platformRef
+  .bootstrapModule(MainModule)
+  .then(() => {
+    console.log('done!');
+
+  })
+);
