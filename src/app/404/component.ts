1457d95e37514d960fe915d093f7f473a5b08056
Zach <zoo.york4130@gmail.com>
2016-09-21 17:38:50 -0500

    404
    @josiah1888 will you hook this 404 up so I can move forward with this
    later tonight/in the morning please.

---
+++ src/app/404/component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '404',
+  styles: [require('./styles.scss').toString()],
+  templateUrl: 'template.html'
+})
+export class 404  {
+}
