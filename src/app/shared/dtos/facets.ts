cf96f1ae305dbaad6493e612c54013a377c9f33e
josiah1888 <josiah@jburchard.com>
2016-10-31 14:11:59 -0500

    fix(filters): set default filters

--- src/app/shared/dtos/facets.ts
+++ src/app/shared/dtos/facets.ts
@@ -1,23 +1,12 @@
 import { Amenity, Location } from './property';
 
 export class PropertyFacet {
-  public min_price: number;
-  public max_price: number;
-  public min_bedrooms: number;
-  public min_bathrooms: number;
-  public max_lease_length: number;
-  public locations: Location[];
-  public amenities: Amenity[];
-  public types: string[];
-
-  constructor() {
-    this.min_price = 0;
-    this.max_price = 25000;
-    this.min_bedrooms = 0;
-    this.min_bathrooms = .5;
-    this.max_lease_length = null;
-    this.locations = [];
-    this.amenities = [];
-    this.types = [];
-  }
+  public min_price: number = 0;
+  public max_price: number = 25000;
+  public min_bedrooms: number = 1;
+  public min_bathrooms: number = 1;
+  public max_lease_length: number = null;
+  public locations: Location[] = [];
+  public amenities: Amenity[] = [];
+  public types: string[] = [];
 }
