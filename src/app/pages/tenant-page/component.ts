2136ab4641b2183fcb8f2d00cc158adb0a1bdb76
Josiah Burchard <josiah@jburchard.com>
2016-11-30 11:32:45 -0600

    style(landing-pages)

--- src/app/pages/tenant-page/component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'landing-page-tenant',
-  styles: [require('./styles.scss').toString()],
-  template: require('./template.html').toString(),
-})
-export class LandingPageTenant {
-}
