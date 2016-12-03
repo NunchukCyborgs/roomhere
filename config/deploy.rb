1c4fb532267a0d707ea6c09cb083d33e57c5ff64
Josiah Burchard <josiah@jburchard.com>
2016-12-03 15:52:45 -0600

    hotfix(deploy): only keep current release

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :passenger_restart_with_touch, true
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-set :keep_releases, 2
+set :keep_releases, 1
 #
 
 namespace :deploy do
