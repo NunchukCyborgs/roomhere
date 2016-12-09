63299ae1bcf010c8de1304b63b5a68286d3d4393
Josiah Burchard <josiah@jburchard.com>
2016-12-09 14:51:35 -0600

    feat(prop-picker): debounce input

--- src/+app/shared/components/property-picker/component.ts
+++ src/+app/shared/components/property-picker/component.ts
@@ -1,6 +1,7 @@
 import { Component, Output, EventEmitter, ViewEncapsulation, ElementRef } from '@angular/core';
 import { isBrowser } from 'angular2-universal';
 import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Property } from '../../dtos/property';
 import { PropertyService } from '../../services/property.service';
 
@@ -14,16 +15,24 @@ export class PropertyPicker {
 
   public properties: Property[] = [];
   public property: Property = new Property();
+  private loadProperties$: BehaviorSubject<string> = new BehaviorSubject('');
 
   constructor(private propertyService: PropertyService, private element: ElementRef) { }
+
+  ngOnInit() {
+    this.loadProperties$
+      .filter(i => Boolean(i && i.length))
+      .debounceTime(250)
+      .flatMap(i => this.propertyService.searchProperties({ query: i }))
+      .filter(i => Boolean(i && i.length))
+      .subscribe(i => this.properties = i)
+  }
 
   ngAfterViewInit() {
     isBrowser && $(this.element.nativeElement).find('input.k-input').attr('placeholder', 'Search Properties ...');
   }
 
   public handleFilter(value) {
-    this.propertyService.searchProperties({ query: value })
-      .filter(i => i && i.length)
-      .subscribe(i => this.properties = i)
+    this.loadProperties$.next(value);
   }
 }
