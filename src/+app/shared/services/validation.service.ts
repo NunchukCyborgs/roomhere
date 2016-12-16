2d2f9cb549fd5bfdf6270b27fe3224d2eb6a88f5
Josiah Burchard <josiah@jburchard.com>
2016-12-16 15:21:13 -0600

    feat(payments): payments validation

--- src/+app/shared/services/validation.service.ts
+++ src/+app/shared/services/validation.service.ts
@@ -13,7 +13,7 @@ export class ValidationService {
   static getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
     let config = {
       'required': 'Required',
-      'maxlength': `Maxium length ${validatorValue.requiredLength}`,
+      'maxlength': `Maximum length ${validatorValue.requiredLength}`,
       'invalidCreditCard': 'Invalid credit card number',
       'invalidEmailAddress': 'Invalid email address',
       // 'invalidPassword': 'Invalid password. Password must be at least 6 characters long, and contain a number.',
@@ -29,8 +29,9 @@ export class ValidationService {
   }
 
   static creditCardValidator(control) {
+    const value = control.value && control.value.replace(/[^0-9]/g, '');
     // Visa, MasterCard, American Express, Diners Club, Discover, JCB
-    if (control.value.match(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/)) {
+    if (value.match(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/)) {
       return null;
     } else {
       return { 'invalidCreditCard': true };
