60a8f23e3b90242f68ebd96419bf93f7c2de22f7
Josiah Burchard <josiah@jburchard.com>
2017-01-03 09:17:20 -0600

    feat(analytics): add guid id

--- src/+app/shared/services/analytics.service.ts
+++ src/+app/shared/services/analytics.service.ts
@@ -7,12 +7,18 @@ import { HttpService, HttpOptions } from './http.service';
 import { UserService } from './user.service';
 import { PropertyService } from './property.service';
 import { PersistenceService } from './persistence.service';
+import { generateGUID } from './util';
 import { PaymentRequest, PaymentRequestBlob } from '../dtos/payment-request';
 
 @Injectable()
 export class AnalyticsService {
   constructor(private router: Router, private http: HttpService, private userService: UserService, private propertyService: PropertyService,
-    private persist: PersistenceService) { }
+    private persist: PersistenceService) {
+    const id = this.persist.get('id');
+    if (!id) {
+      this.persist.set('id', generateGUID());
+    }
+  }
 
   private keenUrl = 'https://api.keen.io/3.0/projects/57ebfd808db53dfda8a711fb/events/';
   private apiKey = '288a891ccc97fce4acf80fddedeab4821adf6f57dd248b629700d7960cedca268e71acbd96b634a434dbf219ce58b029a5fd5dfff6e60acb08c7e1e8441685152ad79af98d65daf0a552bd1a67e2dba828f0f9fa747e01213bd3335a9aa6bc49';
@@ -26,6 +32,7 @@ export class AnalyticsService {
 
   private getBaseOptions(): any {
     return {
+      id: this.persist.get('id'),
       uid: this.persist.get('uid'),
       url: this.router.url,
     };
