2162d0c215cafa037b6bd3b4e7fc88a6227f3889
Stephen Myers <scmyers11@gmail.com>
2017-01-21 14:28:57 -0600

    Change default min beds and baths to 0

--- src/+app/shared/dtos/facets.ts
+++ src/+app/shared/dtos/facets.ts
@@ -6,8 +6,8 @@ export const DEFAULT_MAX_PRICE = 25000;
 export class PropertyFacet {
   public min_price: number = DEFAULT_MIN_PRICE;
   public max_price: number = DEFAULT_MAX_PRICE;
-  public min_bedrooms: number = 1;
-  public min_bathrooms: number = 1;
+  public min_bedrooms: number = 0;
+  public min_bathrooms: number = 0;
   public max_lease_length: number = null;
   public locations: Location[] = [];
   public amenities: Amenity[] = [];
